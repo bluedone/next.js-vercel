bd6230485abac376266f430ead5f87c5485213a3
bluedone <marciajwing@gmail.com>
2021-09-10 13:37:33 +0800

    Blocking fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -29,7 +29,7 @@ export async function getStaticPaths() {
   const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
   client.close();
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => ({
       params: {
         meetupId: meetup._id.toString(),
